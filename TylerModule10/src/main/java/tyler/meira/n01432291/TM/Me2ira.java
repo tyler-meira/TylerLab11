738c2797c1715b5e208484ea8811904b97b3928e
Tyler <tyler.meira@hotmail.ca>
2025-03-27 19:47:32 -0400

    Updated ty1ler Background color

--- TylerModule10/src/main/java/tyler/meira/n01432291/TM/Me2ira.java
+++ TylerModule10/src/main/java/tyler/meira/n01432291/TM/Me2ira.java
@@ -13,7 +13,6 @@ public class Me2ira extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -21,15 +20,6 @@ public class Me2ira extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Me2ira.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Me2ira newInstance(String param1, String param2) {
         Me2ira fragment = new Me2ira();
         Bundle args = new Bundle();
@@ -51,7 +41,8 @@ public class Me2ira extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me2ira, container, false);
+        View view = inflater.inflate(R.layout.fragment_me2ira, container, false);
+
+        return view;
     }
 }
