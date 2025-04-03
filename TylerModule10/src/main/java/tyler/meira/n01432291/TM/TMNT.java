773460082930205e31b442466b560be2a2a8a3d5
Tyler <tyler.meira@hotmail.ca>
2025-04-03 16:04:24 -0400

    Added 5th fragment

---
+++ TylerModule10/src/main/java/tyler/meira/n01432291/TM/TMNT.java
@@ -0,0 +1,49 @@
+package tyler.meira.n01432291.TM;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TMNT extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public TMNT() {
+        // Required empty public constructor
+    }
+
+    public static TMNT newInstance(String param1, String param2) {
+        TMNT fragment = new TMNT();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_t_m_n_t, container, false);
+    }
+}
